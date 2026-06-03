552c53eeee7a94050d32f22e7c8b67cf1ec26a30
Oluotanmi oluwabukunmi Gideon <121802503+Oluotanmi@users.noreply.github.com>
2026-06-04 00:56:07 +0100

    Update vite.config.js

--- Rent-a-ride/vite.config.js
+++ Rent-a-ride/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+//import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      tailwindcss()
+      //tailwindcss()
     ],
     optimizeDeps: {
       include: ["sweetalert2"],
