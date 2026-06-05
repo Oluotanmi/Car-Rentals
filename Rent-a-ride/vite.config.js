9c3adcf596ec9cfde6014652d952c6ecb7c65c38
oluotanmi <fatanmin@yahoo.com>
2026-06-05 11:23:54 +0100

    Update vite config

--- Rent-a-ride/vite.config.js
+++ Rent-a-ride/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
+// import react from '@vitejs/plugin-react'
+
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -8,7 +10,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      tailwindcss()
+      // tailwindcss()
     ],
     optimizeDeps: {
       include: ["sweetalert2"],
